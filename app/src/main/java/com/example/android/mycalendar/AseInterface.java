cd42110765086c745878fcfd9b13f7aa1756b5a5
kaahsh386 <kaahsh38@bu.edu>
2017-12-11 16:09:20 -0500

    progress

---
+++ app/src/main/java/com/example/android/mycalendar/AseInterface.java
@@ -0,0 +1,8 @@
+package com.example.android.mycalendar;
+
+
+public interface AseInterface {
+    public void saveClick(String name);
+    public void cancelClick();
+    public void timechange(int time);
+}
---
+++ app/src/main/java/com/example/android/mycalendar/HelpActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.mycalendar;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HelpActivity extends Activity implements View.OnClickListener {
+    Button returnMenu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+        returnMenu=(Button)findViewById(R.id.help_button);
+        returnMenu.setOnClickListener(this);
+    }
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        Intent x=new Intent(this,CourseActivity.class);
+        x.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(x);
+    }
+
+}
